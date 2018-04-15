7752001c5cc436094dacd96a8b81999b3d314ffc
mkiken <mkiken0@gmail.com>
2018-04-16 00:44:24 +0900

    [bot-gang] 処理改良

--- bot-gang/main.js
+++ bot-gang/main.js
@@ -2,44 +2,40 @@ const TOP_URL = "http://gang-trump.gree-pf.net/";
 
 const METHODS = {
     jump_top: function (response_callback) {
-      jump(TOP_URL);
       response_callback();
+      jump(TOP_URL);
     },
     go_mypage: function (response_callback) {
-      click_element('div.mypage_btn > a');
-      response_callback();
+      click_element(
+        'li.btn-mypage > a',
+        response_callback,
+        click_element.bind(this, 'div.mypage_btn > a', response_callback)
+      )
+
     },
     go_gacha: function (response_callback) {
-      click_element('div.ico-gacha > a');
-      response_callback();
+      click_element('div.ico-gacha > a', response_callback);
     },
     go_gacha_normal_tab: function (response_callback) {
-      click_element('div#gacha > ul > li:nth-child(3) > a');
-      response_callback();
+      click_element('div#gacha > ul > li:nth-child(3) > a', response_callback);
     },
     draw_10000_kizuna_gacha: function (response_callback) {
-      click_element('a.btn-gacha');
-      response_callback();
+      click_element('a.btn-gacha', response_callback);
     },
     go_raid_list: function (response_callback) {
-      click_element('div.ico-resque.ico-new > a');
-      response_callback();
+      click_element('div.ico-resque.ico-new > a', response_callback);
     },
     go_raid_help: function (response_callback) {
-      click_element('div.o-float-c > a');
-      response_callback();
+      click_element('div#raid div.o-float-c > a', response_callback);
     },
     attack_raid: function (response_callback) {
-      click_element('div#raid div.o-talign-c.o-mt-10 > a');
-      response_callback();
+      click_element('div#raid div.o-talign-c.o-mt-10 > a', response_callback);
     },
     go_own_raid: function (response_callback) {
-      click_element('div.ico-appear > a');
-      response_callback();
+      click_element('div.ico-appear > a', response_callback);
     },
     request_raid_help: function (response_callback) {
-      click_element('div#raid div.o-mt-10.o-talign-c > a');
-      response_callback();
+      click_element('div#raid div.o-mt-10.o-talign-c > a', response_callback);
     },
 };
 
@@ -68,10 +64,18 @@ function createLogMessage(msg) {
   return (new Date()).toString() + ": " + msg;
 }
 
-function click_element(selector) {
+function click_element(selector, success_callback, fail_callback = null) {
    let element = get_element(selector);
    if (element) {
+     success_callback();
      element.click();
+     return;
+   }
+
+   if (fail_callback) {
+     fail_callback();
+   } else {
+     success_callback();
    }
 }
 
