09cad49bfc00c605a1922ccfaaf4b6043574d70c
mkiken <mkiken0@gmail.com>
2023-06-25 23:18:47 +0900

    [bot-takarazuka] ログインフォーム

--- bot-takarazuka/run.py
+++ bot-takarazuka/run.py
@@ -1,17 +1,33 @@
 # ライブラリ読み込み
-from selenium import webdriver
+import json
 import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 # クロームの立ち上げ
 driver = webdriver.Chrome()
+wait = WebDriverWait(driver, 15)
 
 # ページ接続
-driver.get('https://www.takarazuka-ticket.com/mp/twjlg.do?md=1&ls=2&ul=https://www.takarazuka-ticket.com/rt/twjkl.do')
+driver.get(
+    'https://www.takarazuka-ticket.com/mp/twjlg.do?md=1&ls=2&ul=https://www.takarazuka-ticket.com/rt/twjkl.do')
 
 # ログインする
 
-# 決定ボタンのクリック
-# driver.find_element_by_xpath('//*[@id="editAccountForm"]/p[2]').click()
+# ログインフォーム
+login_id_form = wait.until(EC.element_to_be_clickable((By.ID, 'loginid_form')))
+
+with open('credential.json') as f:
+    d = json.load(f)
+
+login_id_form.send_keys(d['id'])
+driver.find_element(By.ID, 'password_form').send_keys(d['password'])
+
+login_button = wait.until(EC.element_to_be_clickable((By.ID, 'login_btn')))
+login_button.click()
 
 # 2時間終了を待つ
 time.sleep(7200)
